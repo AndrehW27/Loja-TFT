b1cede25359d29f187bcf9ad4a72342d9e4e0422
André Willian Carvalho <andrehwillian@hotmail.com>
2021-05-10 01:18:54 -0300

    moldura, botoes e icones prontos

--- src/components/Store.js
+++ src/components/Store.js
@@ -11,10 +11,26 @@ export default function Bottom() {
     const [championBox4, SetChampionBox4] = useState(1);
     const [championBox5, SetChampionBox5] = useState(1);
 
-    const levelUp = () => {
-        SetLevel(level + 1)
-    }
+    const level1Arr = [100, 0, 0, 0, 0];
+    const level2Arr = [100, 0, 0, 0, 0];
+    const level3Arr = [75, 25, 0, 0, 0];
+    const level4Arr = [55, 30, 15, 0, 0];
+    const level5Arr = [45, 33, 20, 2, 0];
+    const level6Arr = [35, 35, 25, 5, 0];
+    const level7Arr = [19, 35, 30, 15, 1];
+    const level8Arr = [15, 20, 35, 25, 5];
+    const level9Arr = [10, 15, 30, 30, 15];
 
+    const levelUp = () => {
+        if (level < 9) {
+            SetLevel(level + 1)
+        }
+    }
+    const levelDown = () => {
+        if (level > 1) {
+            SetLevel(level - 1)
+        }
+    }
     const resetLevel = () => {
         SetLevel(1)
     }
@@ -41,14 +57,44 @@ export default function Bottom() {
         <div className="containerStore">
 
             <div className="levelBar">
+                <div className="level">
+                    <div className="levelNumber">
+                        <p>Lvl. {level}</p>
+                        <p>12/50</p>
+                    </div>
+                    <div className="levelUpgrade">
+                        <div className="levelUpgradeIn"></div>
+                    </div>
+
+                </div>
+                <div className="odds">
+                    <div className="oneStar">{level1Arr[0]}%</div>
+                    <div className="twoStar">{level1Arr[1]}%</div>
+                    <div className="threeStar">{level1Arr[2]}%</div>
+                    <div className="fourStar">{level1Arr[3]}%</div>
+                    <div className="fiveStar">{level1Arr[4]}%</div>
+                </div>
+                <div className="gold">
+                    <div className="goldIcon"></div>
+                    <div className="goldValue">50</div>
+                </div>
+                <button type="button" className="lock">
+                    <div className="lockIconClosed"></div>
+                    <div className="lockIconOpen"></div>
+                </button>
             </div>
 
             <div className="store">
                 <div className="levelBox">
-                    <p>Level: {level}</p>
-                    <button type="button" onClick={levelUp}>Level up</button>
-                    <button type="button" onClick={resetLevel}>Reset level</button>
-                    <button type="button" onClick={changeBoxNumbers}>Refresh Store</button>
+
+                    <div className="myButtonLvl">
+                        <button className="levelUp" type="button" onClick={levelUp}>Up</button>
+                        <button className="levelDown" onClick={levelDown}>Down</button>
+                        <button className="resetLevel" onClick={resetLevel}>Reset</button>
+                    </div>
+
+                    <button className="myButton buy" type="button" >Buy XP</button>
+                    <button className="myButton refresh" type="button" onClick={changeBoxNumbers}>Refresh</button>
                 </div>
 
                 <div className="championBox" id="1">{championBox1}</div>
