45ae969ca697a84a956542bad28e914f0b29bd40
André Willian Carvalho <andrehwillian@hotmail.com>
2021-05-12 00:02:33 -0300

    cores, estrelas e icones mudando no botao refresh

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from 'react';
 import '../styles/store.css';
 
-import imgInicial from '../assets/tft-battle-field.jpg';
-import ziggsImg from '../assets/champs/ziggs.jpg';
+import imgInicial from '../assets/tft.jpg';
 
 import paperSound from '../assets/paper.mp3';
 import paperSound2 from '../assets/paper2.mp3';
@@ -31,7 +30,7 @@ export default function Bottom() {
     }
 
     // const [champions, setChampions] = useState([]);
-    const [gold, setGold] = useState(50);
+    const [gold, setGold] = useState(50);    
     const [level, setLevel] = useState(1);
     const [currentExp, setCurrentExp] = useState(0);
     const [maxExp, setMaxExp] = useState(2);
@@ -43,20 +42,36 @@ export default function Bottom() {
     const [oddsFiveStar, setOddsFiveStar] = useState(0);
 
 
-    const [championBox1, setChampionBox1] = useState("");
+    const [championNome1, setChampionNome1] = useState("");
     const [champImg1, setChampImg1] = useState(imgInicial);
+    const [starNumber1, setStarNumber1] = useState();
+    const [cor1, setCor1] = useState("rgba(21, 54, 54, 0.836)");
+    const [corBorder1, setCorBorder1] = useState("rgba(21, 54, 54, 0.836)");
 
-    const [championBox2, setChampionBox2] = useState("");
+    const [championNome2, setChampionNome2] = useState("");
     const [champImg2, setChampImg2] = useState(imgInicial);
+    const [starNumber2, setStarNumber2] = useState();
+    const [cor2, setCor2] = useState("rgba(21, 54, 54, 0.836)");
+    const [corBorder2, setCorBorder2] = useState("rgba(21, 54, 54, 0.836)");
 
-    const [championBox3, setChampionBox3] = useState("");
+    const [championNome3, setChampionNome3] = useState("");
     const [champImg3, setChampImg3] = useState(imgInicial);
+    const [starNumber3, setStarNumber3] = useState();
+    const [cor3, setCor3] = useState("rgba(21, 54, 54, 0.836)");
+    const [corBorder3, setCorBorder3] = useState("rgba(21, 54, 54, 0.836)");
 
-    const [championBox4, setChampionBox4] = useState("");
+    const [championNome4, setChampionNome4] = useState("");
     const [champImg4, setChampImg4] = useState(imgInicial);
+    const [starNumber4, setStarNumber4] = useState();
+    const [cor4, setCor4] = useState("rgba(21, 54, 54, 0.836)");
+    const [corBorder4, setCorBorder4] = useState("rgba(21, 54, 54, 0.836)");
 
-    const [championBox5, setChampionBox5] = useState("");
+    const [championNome5, setChampionNome5] = useState("");
     const [champImg5, setChampImg5] = useState(imgInicial);
+    const [starNumber5, setStarNumber5] = useState();
+    const [cor5, setCor5] = useState("rgba(21, 54, 54, 0.836)");
+    const [corBorder5, setCorBorder5] = useState("rgba(21, 54, 54, 0.836)");
+
     
     // let championsvar = JSON.stringify(championsArr[0]);
     // setChampions(championsvar);
@@ -106,47 +121,82 @@ export default function Bottom() {
         }
     }
 
-
-
-
-    let champs1 = [{ nome: "Ziggs", imagem: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ziggs_7.jpg" },
-    { nome: "Kha'Zix", imagem: "https://arenaesports.com.br/wp-content/uploads/2018/09/Kha-Zix-campeonato.jpg" },
-    { nome: "Leona", imagem: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leona_10.jpg" }]
+    let champs1 = [{ nome: "Ziggs", star:1, cor:"rgba(58, 58, 87, 0.836)", imagem: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ziggs_7.jpg" },
+    { nome: "Kha'Zix", star:2, cor:"rgba(17, 83, 52, 0.836)", imagem: "https://arenaesports.com.br/wp-content/uploads/2018/09/Kha-Zix-campeonato.jpg" },
+    { nome: "Leona", star:3, cor:"rgba(30, 72, 121, 0.836)", imagem: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Leona_10.jpg" }]
 
     // ATUALIZAR LOJA
     const refreshStore = () => {
+
+        document.getElementById("iconInicial1").style.display = "flex";
+        document.getElementById("iconInicial2").style.display = "flex";
+        document.getElementById("iconInicial3").style.display = "flex";
+        document.getElementById("iconInicial4").style.display = "flex";
+        document.getElementById("iconInicial5").style.display = "flex";
 
         // LEVEL 1
         if (level === 1 && gold > 1) {
             let randomBox1 = Math.floor(Math.random() * 3);
             let novoChampBox1 = champs1[randomBox1].nome;
             let novoImg1 = champs1[randomBox1].imagem;
+            let novoStar1 = champs1[randomBox1].star;
+            let novoCor1 = champs1[randomBox1].cor;
+            let novoCorBorder1 = champs1[randomBox1].cor;
+            setCorBorder1(novoCorBorder1);
+            setCor1(novoCor1);
             setChampImg1(novoImg1);
-            setChampionBox1(novoChampBox1);
+            setChampionNome1(novoChampBox1);
+            setStarNumber1(novoStar1);
+            
 
             let randomBox2 = Math.floor(Math.random() * 3);
             let novoChampBox2 = champs1[randomBox2].nome;
             let novoImg2 = champs1[randomBox2].imagem;
+            let novoStar2 = champs1[randomBox2].star;
+            let novoCor2 = champs1[randomBox2].cor;
+            let novoCorBorder2 = champs1[randomBox2].cor;
+            setCorBorder2(novoCorBorder2);
+            setCor2(novoCor2);
             setChampImg2(novoImg2);
-            setChampionBox2(novoChampBox2);
+            setChampionNome2(novoChampBox2);
+            setStarNumber2(novoStar2);
+
 
             let randomBox3 = Math.floor(Math.random() * 3);
             let novoChampBox3 = champs1[randomBox3].nome;
             let novoImg3 = champs1[randomBox3].imagem;
+            let novoStar3 = champs1[randomBox3].star;
+            let novoCor3 = champs1[randomBox3].cor;
+            let novoCorBorder3 = champs1[randomBox3].cor;
+            setCorBorder3(novoCorBorder3);
+            setCor3(novoCor3);
             setChampImg3(novoImg3);
-            setChampionBox3(novoChampBox3);
+            setChampionNome3(novoChampBox3);
+            setStarNumber3(novoStar3);            
 
             let randomBox4 = Math.floor(Math.random() * 3);
             let novoChampBox4 = champs1[randomBox4].nome;
             let novoImg4 = champs1[randomBox4].imagem;
+            let novoStar4 = champs1[randomBox4].star;
+            let novoCor4 = champs1[randomBox4].cor;
+            let novoCorBorder4 = champs1[randomBox4].cor;
+            setCorBorder4(novoCorBorder4);
+            setCor4(novoCor4);
             setChampImg4(novoImg4);
-            setChampionBox4(novoChampBox4);
+            setChampionNome4(novoChampBox4);
+            setStarNumber4(novoStar4);  
 
             let randomBox5 = Math.floor(Math.random() * 3);
             let novoChampBox5 = champs1[randomBox5].nome;
             let novoImg5 = champs1[randomBox5].imagem;
+            let novoStar5 = champs1[randomBox5].star;
+            let novoCor5 = champs1[randomBox5].cor;
+            let novoCorBorder5 = champs1[randomBox5].cor;
+            setCorBorder5(novoCorBorder5);
+            setCor5(novoCor5);
             setChampImg5(novoImg5);
-            setChampionBox5(novoChampBox5);
+            setChampionNome5(novoChampBox5);
+            setStarNumber5(novoStar5);       
 
             if (gold > 1) {
                 setGold(gold - 2)
@@ -154,26 +204,7 @@ export default function Bottom() {
         }
 
         // LEVEL 2
-        else if (level === 2) {
-            let randomBox1 = Math.floor(Math.random() * (6 - 3)) + 3;
-            setChampionBox1(randomBox1);
 
-            let randomBox2 = Math.floor(Math.random() * (6 - 3)) + 3;
-            setChampionBox2(randomBox2);
-
-            let randomBox3 = Math.floor(Math.random() * (6 - 3)) + 3;
-            setChampionBox3(randomBox3);
-
-            let randomBox4 = Math.floor(Math.random() * (6 - 3)) + 3;
-            setChampionBox4(randomBox4);
-
-            let randomBox5 = Math.floor(Math.random() * (6 - 3)) + 3;
-            setChampionBox5(randomBox5);
-
-            if (gold > 1) {
-                setGold(gold - 2)
-            }
-        }
 
 
     }
@@ -335,53 +366,53 @@ export default function Bottom() {
                     </button>
                 </div>
 
-                <div className="championBox" id="box1">
+                <div className="championBox" id="box1" style={{borderColor: corBorder1}}>
                     <img src={champImg1} className="champImg"></img>
-                    <div className="champNome1">
-                        {championBox1}
+                    <div className="champNome1" style={{backgroundColor: cor1}}>
+                        {championNome1}
                         <div className="valueAndIconContainer">
-                            <div className="buyGoldIcon"></div>
-                            <p className="buyValue">1</p>
+                            <div id="iconInicial1" className="buyGoldIcon iconInicial"></div>
+                            <p className="buyValue">{starNumber1}</p>
                         </div>
                     </div>
                 </div>
-                <div className="championBox" id="box2">
+                <div className="championBox" id="box2" style={{borderColor: corBorder2}}>
                     <img src={champImg2} className="champImg"></img>
-                    <div className="champNome2">
-                        {championBox2}
+                    <div className="champNome2" style={{backgroundColor: cor2}}>
+                        {championNome2}
                         <div className="valueAndIconContainer">
-                            <div className="buyGoldIcon"></div>
-                            <p className="buyValue">1</p>
+                            <div id="iconInicial2" className="buyGoldIcon"></div>
+                            <p className="buyValue">{starNumber2}</p>
                         </div>
                     </div>
                 </div>
-                <div className="championBox" id="box3">
+                <div className="championBox" id="box3" style={{borderColor: corBorder3}}>
                     <img src={champImg3} className="champImg"></img>
-                    <div className="champNome3">
-                        {championBox3}
+                    <div className="champNome3" style={{backgroundColor: cor3}}>
+                        {championNome3}
                         <div className="valueAndIconContainer">
-                            <div className="buyGoldIcon"></div>
-                            <p className="buyValue">1</p>
+                            <div id="iconInicial3" className="buyGoldIcon"></div>
+                            <p className="buyValue">{starNumber3}</p>
                         </div>
                     </div>
                 </div>
-                <div className="championBox" id="box4">
+                <div className="championBox" id="box4" style={{borderColor: corBorder4}}>
                     <img src={champImg4} className="champImg"></img>
-                    <div className="champNome4">
-                        {championBox4}
+                    <div className="champNome4" style={{backgroundColor: cor4}}>
+                        {championNome4}
                         <div className="valueAndIconContainer">
-                            <div className="buyGoldIcon"></div>
-                            <p className="buyValue">1</p>
+                            <div id="iconInicial4" className="buyGoldIcon"></div>
+                            <p className="buyValue">{starNumber4}</p>
                         </div>
                     </div>
                 </div>
-                <div className="championBox" id="box5">
+                <div className="championBox" id="box5" style={{borderColor: corBorder5}}>
                     <img src={champImg5} className="champImg"></img>
-                    <div className="champNome5">
-                        {championBox5}
+                    <div className="champNome5" style={{backgroundColor: cor5}}>
+                        {championNome5}
                         <div className="valueAndIconContainer">
-                            <div className="buyGoldIcon"></div>
-                            <p className="buyValue">1</p>
+                            <div id="iconInicial5" className="buyGoldIcon"></div>
+                            <p className="buyValue">{starNumber5}</p>
                         </div>
                     </div>
                 </div>
